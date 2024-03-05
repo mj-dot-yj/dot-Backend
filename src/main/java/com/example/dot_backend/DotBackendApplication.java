509c8791643696d79d13193505c292e4fc22baa7
minjeong.kim <kminjeong01@naver.com>
2024-03-05 09:03:54 +0900

    feat : 초기 개발 환경

---
+++ src/main/java/com/example/dot_backend/DotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.dot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dot_backend/DotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
