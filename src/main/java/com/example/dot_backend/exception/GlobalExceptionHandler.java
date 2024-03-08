c8f0e09590ea5fc7871f0178ba011b061f0a803f
minjeong.kim <kminjeong01@naver.com>
2024-03-08 14:40:12 +0900

    fix : 예외 처리 수정

--- src/main/java/com/example/dot_backend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/dot_backend/exception/GlobalExceptionHandler.java
@@ -2,7 +2,9 @@ package com.example.dot_backend.exception;
 
 import com.example.dot_backend.common.ApiResponse;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,6 +13,10 @@ import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseEntity<ApiResponse<String>> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiResponse.onFailure(ex.getMessage()));
+    }
 
     @ExceptionHandler({MethodArgumentNotValidException.class})
     public ResponseEntity<ApiResponse<String>> handleMethodArgumentNotValidHandler(MethodArgumentNotValidException ex) {
@@ -19,5 +25,10 @@ public class GlobalExceptionHandler {
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .toList();
         return ResponseEntity.status(ex.getStatusCode()).body(ApiResponse.onFailure(errors.get(0)));
+    }
+
+    @ExceptionHandler({AuthenticationException.class})
+    public ResponseEntity<ApiResponse<String>> handleAuthenticationException(AuthenticationException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiResponse.onFailure("Credentials failed"));
     }
 }
